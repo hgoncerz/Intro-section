809e304301f326736bde86d576ace9d738409334
hgoncerz <hubertg489655@gmail.com>
2022-08-04 16:27:14 +0200

    added dropdwon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../scss/navbar/navbar.scss";
-import { FiChevronDown } from "react-icons/fi";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import DropDownFeatures from "./DropDownFeatures";
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <nav className="navbar">
       <h1>snap</h1>
-      <a href="#" style={{ display: "flex" }}>
+      {/* <a
+        href="#"
+        style={{ display: "flex" }}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
         Features{" "}
-        <FiChevronDown
-          size="18px"
-          style={{ display: "flex", marginLeft: "2px" }}
-        />
+        {isOpen ? (
+          <FiChevronDown
+            size="18px"
+            style={{ display: "flex", marginLeft: "2px" }}
+          />
+        ) : (
+          <FiChevronUp
+            size="18px"
+            style={{ display: "flex", marginLeft: "2px" }}
+          />
+        )}
       </a>
+      {isOpen && <DropDownFeatures />} */}
+      <DropDownFeatures isOpen={isOpen} setIsOpen={setIsOpen} />
       <a href="#" style={{ display: "flex" }}>
         Company{" "}
         <FiChevronDown
